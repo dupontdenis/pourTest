6b9c621ae063f2f78343fe425b8a16e99200caba
Super Dupont <dupontdenis@users.noreply.github.com>
2024-11-10 17:15:45 +0100

    Update applyCeilMarks.mjs

--- applyCeilMarks.mjs
+++ applyCeilMarks.mjs
@@ -1,10 +1,3 @@
 export function applyCeilMarks(students) {
-  return students.map((student) => {
-    const ceil = student.marks.some((mark) => mark < 7);
-    if (ceil) {
-      return { ...student, ceil };
-    } else {
-      return { ...student };
-    }
-  });
+  // return 
 }
