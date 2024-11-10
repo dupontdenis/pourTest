342bff3fe0176b456d4c58862f882b22d17b4a4f
denis dupont <denis.dupont@univ-evry.fr>
2024-11-10 15:23:16 +0100

    first commit

---
+++ renderTable.mjs
@@ -0,0 +1,33 @@
+export function generateTableHTML(studentData) {
+  if (studentData.length === 0) return "";
+
+  const headers = Object.keys(studentData[0]);
+
+  const tableHTML = `
+    <table class="table table-hover table-bordered">
+      <thead class="thead-dark">
+        <tr>
+          ${headers.map((header) => `<th>${header}</th>`).join("")}
+        </tr>
+      </thead>
+      <tbody>
+        ${studentData.map(
+          (student) => `
+            <tr>
+              ${headers
+                .map((header) => {
+                  return `<td>${
+                    Array.isArray(student[header])
+                      ? student[header].join(", ")
+                      : student[header]
+                  }</td>`;
+                })
+                .join("")}
+            </tr>`
+        ).join("")}
+      </tbody>
+    </table>
+  `;
+
+  return tableHTML;
+}
