e33e87c0ab078fc6074512f532a31c0451795ad6
Super Dupont <dupontdenis@users.noreply.github.com>
2024-11-10 17:17:21 +0100

    Update computeStudentStatistics.mjs

--- computeStudentStatistics.mjs
+++ computeStudentStatistics.mjs
@@ -1,9 +1,9 @@
 export function computeStudentStatistics(students) {
   return students.map((student) => {
-    const totalMarks = student.marks.reduce((sum, mark) => sum + mark, 0);
+    // const totalMarks = 
     const averageMarks = (totalMarks / student.marks.length).toFixed(2);
-    const ceil = student.marks.some((mark) => mark < 7) ? true : "";
+    // const ceil = 
 
-    return { ...student, averageMarks, ceil };
+    // return { };
   });
 }
